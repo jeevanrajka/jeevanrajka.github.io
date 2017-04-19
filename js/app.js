5b2d345ec987217cf6130daa6701c7655cca1888
jeevanrajka <jk00493806@techmahindra.com>
2017-04-19 19:21:16 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,50 +18,62 @@ example.config(function ($stateProvider, $urlRouterProvider) {
 example.controller("HttpGetController", function ($scope, $http) {
 
     $scope.authCode = (document.URL).split("=")[1];
-    console.log($scope.authCode);
+    //$scope.authCode = $scope.authCode.split("&")[0];
+   // alert($scope.authCode);
     if ($scope.authCode !== undefined && $scope.authCode !== null) {
 
 
 
         // use $.param jQuery function to serialize data from JSON 
-   /*     $scope.url = "grant_type=authorization_code&code=" + $scope.authCode + "&client_id=3MVG9d8..z.hDcPLYDRVrSqJ5pWGR1jR_nr62gB4mFDdhczCF88mUN3hSc0kpWqMCAgmjE1Ci9kyBPvhN892H&client_secret=484651292492712585&redirect_uri=https://jeevanrajka.github.io/oauth_callback.html"
-        console.log($scope.url);
+        $scope.postData = "grant_type=authorization_code&code=" + $scope.authCode + "&client_id=3MVG9d8..z.hDcPLYDRVrSqJ5pcFcQCPXiP58IRC2vsAuzN0mKBzj.JT2UsxLrbPyWM.parNXJDbz3sbvKgdu&client_secret=5679420362518686539&redirect_uri=http://localhost/oauth/oauth_callback.html"
+      // scope.postData = "grant_type=refresh_token&client_id=3MVG9d8..z.hDcPLYDRVrSqJ5pWGR1jR_nr62gB4mFDdhczCF88mUN3hSc0kpWqMCAgmjE1Ci9kyBPvhN892H&refresh_token=5Aep8613hy0tHCYdhyrLKTOqkGaICl.XlyhckFVF1waP8Z46N4WkdMfkRig7rbyPvAWFxTxh31dyLUvBIa8ZsHt&format=json"
+        console.log("$scope.postData:::",$scope.postData);
 
 
-       $http.post('https://login.salesforce.com/services/oauth2/token', $scope.url)
-        .success(function(successData){
-          console.log(successData.id);
-        }); 
-        
+       /*    $http.post('https://login.salesforce.com/services/oauth2/token', $scope.postData)
+           .success(function(successData){
+             alert("hi");
+           });*/
 
+/*var dataURL1  = "https://login.salesforce.com/services/oauth2/token";
+var x = dataURL1 + "grant_type=authorization_code&code=";
+console.log("x:",x);
 
-             /*   $http({
-                method : "GET",
-                url : "https://login.salesforce.com/services/oauth2/token?" + $scope.url
-            }).then(function mySucces(response) {
-                $scope.myWelcome = response.data;
-            }, function myError(response) {
-                $scope.myWelcome = response.statusText;
-                console.log(response.data.id);
-            }); */
-    
-    
-   /*     if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        }
-        else {// code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.open("GET", "https://login.salesforce.com/services/oauth2/token?"+$scope.url, true);
-        xmlhttp.send();
-        xmlDoc = xmlhttp.responseXML;
+var aduthCode = x + $scope.authCode;
+console.log("aduthCode:",aduthCode);
 
-        $scope.id = xmlDoc.getElementByTagName("id");
-        console.log($scope.id);
-        */
+console.log("dataUrl ::",dataURL1);
 
-       // window.location.href = "https://jeevanrajka.github.io/#/secure";
-       // https://jeevanrajka.github.io/oauth_callback.html
+var dataURL2 =  "&client_id=3MVG9d8..z.hDcPLYDRVrSqJ5pcFcQCPXiP58IRC2vsAuzN0mKBzj.JT2UsxLrbPyWM.parNXJDbz3sbvKgdu&client_secret=5679420362518686539&redirect_uri=http://localhost/oauth/oauth_callback.html";
+console.log("dataUrl ::",dataURL2);
+
+var DataUrl = dataURL2 + dataURL2;
+console.log("dataUrl ::",DataUrl);*/
+$scope.a = "https://login.salesforce.com/services/oauth2/token?"+$scope.postData;
+alert($scope.a);
+var req = {
+    method: 'POST',
+    url: "https://login.salesforce.com/services/oauth2/token",
+    crossDomain: true,
+    headers: {
+        'Content-Type': 'application/json' , 
+	       'Access-Control-Allow-Origin': '*',
+	        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+	      'Access-Control-Allow-Headers':'X-Requested-With'	
+    },
+    params: {   grant_type : "authorization_code", 
+                code : $scope.authCode,
+                client_id : "3MVG9d8..z.hDcPLYDRVrSqJ5pcFcQCPXiP58IRC2vsAuzN0mKBzj.JT2UsxLrbPyWM.parNXJDbz3sbvKgdu",
+                client_secret : 5679420362518686539,
+                redirect_uri : "http://localhost/oauth/oauth_callback.html"
+              }
+}
+
+$http(req).then(function (responce) {
+    alert("success");
+}, function (responce) {
+    alert("error");
+});
         
         
 
